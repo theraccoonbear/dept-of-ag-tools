60dd14abb5945148f11f0a0f4358d8afcc53aadf
Don Smith <smith@earthlinginteractive.com>
2017-05-05 14:24:07 -0500

    debugging

--- js/agreement-mapping.js
+++ js/agreement-mapping.js
@@ -1,23 +1,47 @@
 $(function() {
+	var logMsg = function(m) {
+		if (false && console) {
+			console.log(m);
+		} else {
+			if (typeof m !== 'string' && typeof m !== 'number') {
+				if (JSON && JSON.stringify) {
+					alert(JSON.stringify(m, null, 2));
+				} else {
+					alert(m);
+				}
+			} else {
+				alert(m);
+			}
+		}
+	}
+
 	$make_records.on('click', function(e) {
+		var cancelling = false;
 		var row_data = $sheet_data
 			.val()
 			.trim()
 			.split(/\n/g);
 		var data = [row_data[0]];
 
-		if (row_data[0].ParcelList === 'undefined') {
+		var columns = row_data[0].split(/\t/);
+		if (columns.ParcelList === 'undefined') {
 			alert("You must include the column label row in the data you paste.");
 			return;
 		}
 
-		var columns = row_data[0].split(/\t/);
 		$.each(row_data.slice(1), function(idx, row) {
+			if (cancelling) { return; }
 			var fields = row.split(/\t/g);
 			var newRec = {};
 			$.each(fields, function(fidx, f) {
 				newRec[columns[fidx]] = f;
 			});
+
+			if (typeof newRec.ParcelList === 'undefined') {
+				logMsg("No parcel list in:\n\n" + row);
+				cancelling = true;
+				return;
+			}
 
 			var pNums1 = newRec.ParcelList.split(/,\s*/g);
 			var pNums2 = newRec.ParcelListMid.split(/,\s*/g);
@@ -38,6 +62,10 @@ $(function() {
 				}
 			});
 		});
-		$output_records.val(data.join("\n"));
+		if (cancelling) {
+			$output_records.val('');
+		} else {
+			$output_records.val(data.join("\n"));
+		}
 	});
 });

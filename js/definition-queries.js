bf9abe9b5709dc0c8032d6402ccfc173865a461b
Don Smith <smith@earthlinginteractive.com>
2017-05-10 13:40:05 -0500

    customize column name

--- js/definition-queries.js
+++ js/definition-queries.js
@@ -3,11 +3,12 @@ $(function() {
     var convert = function() {
         var pnums = [];
         var lines = $parcel_list.val().split(/\n/);
+        var colName = $parcel_column_name.val() || 'PAR_NO'
         
         $.each(lines, function(i, l) {
            var line_pnum = l.split(/,/);
             $.each(line_pnum, function(x, pn) {
-                pnums.push("\"PAR_NO\" = '" + pn.trim() + "'");
+                pnums.push('"' + colName + '" = \'' + pn.trim() + "'");
             });
         });
         $generated_query.val(pnums.join(' OR '));
